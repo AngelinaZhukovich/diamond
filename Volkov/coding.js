4e92e89a23f9f594a821ef9cda3e2f8a592a6bb0
AngelinaZhukovich <angelinka04@mail.ru>
2021-02-01 01:03:26 +0300

    book+

--- Volkov/coding.js
+++ Volkov/coding.js
@@ -1,4 +1,30 @@
-function addItem (){
-    alert ("Нанять охрану!");
+
+ 
+ 
+ function addItem (){
+    
+   alert ("Нанять охрану!");
+   document.addEventListener('button', addItem);
 };
 addItem();
+
+
+
+// document.getElementById("myBtn").addEventListener ("click", addItem);
+
+
+// document.getElementById("myBtn").addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+//   });
+
+
+
+
+
+// var name = "Dr";
+//     if (name == "Dr") {
+//     alert ("Доступ разрешен");
+// }
+//     else {
+//         alert ("Доступ запрещен");
+//     };
