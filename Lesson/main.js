6956d7ac49e430f38161a2f722e23e886d6962c2
AngelinaZhukovich <angelinka04@mail.ru>
2021-03-09 22:03:29 +0300

    1

---
+++ Lesson/main.js
@@ -0,0 +1,12 @@
+
+// типы данных
+let numb = 20; //Number
+let bigIntNum = 900719925474099123n; // BigInt
+let str1 ='Hello' //string
+let variable1 = true; //boolean
+let empty = null; //null
+let test; // undefined
+let user = {id:1, firstName='Angelina'}; //object
+let sym = Symbol(); // Symble
+
+console.log (typeof test); //typeof проверяет тип данных
